9c3781ed9882c1af1671406824e17842ec408ae0
Marina <marinabashkina90@gmail.com>
2017-09-02 21:04:47 +0300

    Clock,Goodlnc

---
+++ Clock/clock.js
@@ -0,0 +1,41 @@
+/*alert('Hello World');
+console.log('Some Messager');
+document.write('Some Messager');
+
+
+var number =10;
+alert(number);
+console.log(number);
+var name = "<h1> Marina</h>";
+document.write(name);
+var a = true;
+console.log(a);
+var b ;
+
+console.log(b);
+var c = null;
+console.log(c);
+console.log(typeof name);
+console.log(typeof number);
+console.log(typeof b);*/
+/*var a =15;
+var b =10;
+var c =a*b;
+
+
+
+document.write(c);
+var c=a-b;
+document.write(c );
+ c=a+b;
+document.write(c);
+c=a/b;
+document.write(c);
+var d=3%2;
+console.log(d);*/
+var i =1;
+var j =1;
+console.log(j++);
+console.log(++i);
+
+
